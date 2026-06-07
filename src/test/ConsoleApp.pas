3654bb1376fa74e728b644b5a02e41d7818c4dd2
Michael Justin <michael@mikejustin.com>
2026-06-07 10:02:01 +0200

    Add Free Pascal console application using Log4D unit

---
+++ src/test/ConsoleApp.pas
@@ -0,0 +1,11 @@
+program ConsoleApp;
+
+{$mode objfpc}{$H+}
+
+uses
+  Log4D;
+
+begin
+  WriteLn('Free Pascal Console Application using Log4D');
+  WriteLn('This application uses the Log4D unit.');
+end.
